098715bcf19d0cf8cd2c39dc6d6f35e23ea27742
ruif3r <ruif3rofficial@gmail.com>
2020-08-23 18:21:12 -0400

    Fixed issues based on PR suggestions, and added real settings persistence, using Preference API

--- app/src/main/java/com/example/ncov19traking/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/example/ncov19traking/ui/settings/SettingsActivity.kt
@@ -1,25 +1,38 @@
 package com.example.ncov19traking.ui.settings
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
 import com.example.ncov19traking.R
 import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val nightMode = "NightMode"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
         val nightModeSwitch = findViewById<SwitchMaterial>(R.id.night_mode_switch)
-        checkSwitchStatus(nightModeSwitch)
+        checkSwitchStatus(nightModeSwitch, preferences)
         nightModeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            preferences.edit().run {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    putBoolean(nightMode, true)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    putBoolean(nightMode, false)
+                }
+                apply()
+            }
         }
     }
 
-    private fun checkSwitchStatus(switch: SwitchMaterial) {
-        switch.isChecked =
-            AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+    private fun checkSwitchStatus(switch: SwitchMaterial, preferences: SharedPreferences) {
+        switch.isChecked = preferences.getBoolean(nightMode, false)
     }
 }
