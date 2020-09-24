6b9557d5ddc18e4b0da7c92d582c4572740e1e7a
ruif3r <ruif3rofficial@gmail.com>
2020-09-23 22:35:17 -0300

    Fixed NotificationWorker following PR indications

--- app/src/main/java/com/example/ncov19traking/NotificationWorker.kt
+++ app/src/main/java/com/example/ncov19traking/NotificationWorker.kt
@@ -8,18 +8,33 @@ import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
+const val NOTIFICATION_ID = 101
+
 class NotificationWorker(context: Context, workerParams: WorkerParameters) : Worker(
     context,
     workerParams
 ) {
     override fun doWork(): Result {
-        val notificationId = 101
-        val intent = Intent(
+        val intent = getMainActivityIntent()
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+        val builder = reminderNotificationBuilder(pendingIntent)
+        showNotification(builder)
+        return Result.success()
+    }
+
+    private fun showNotification(builder: NotificationCompat.Builder) {
+        with(NotificationManagerCompat.from(applicationContext)) {
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    private fun getMainActivityIntent() = Intent(
             applicationContext,
             MainActivity::class.java
         ).apply { Intent.FLAG_ACTIVITY_CLEAR_TASK }
-        val pendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
-        val builder = NotificationCompat.Builder(applicationContext, BaseApp.CHANNEL_ID)
+
+    private fun reminderNotificationBuilder(pendingIntent: PendingIntent?): NotificationCompat.Builder {
+        return NotificationCompat.Builder(applicationContext, BaseApp.CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_notifications_black_24dp)
             .setContentTitle("Reminder")
             .setContentText("Remember to wash your hands for at least 20 seconds!")
@@ -28,10 +43,6 @@ class NotificationWorker(context: Context, workerParams: WorkerParameters) : Wor
                     .bigText("Remember to wash your hands for at least 20 seconds!")
             ).setPriority(NotificationCompat.PRIORITY_DEFAULT).setContentIntent(pendingIntent)
             .setAutoCancel(true)
-        with(NotificationManagerCompat.from(applicationContext)) {
-            notify(notificationId, builder.build())
-        }
-        return Result.success()
     }
 
 }
